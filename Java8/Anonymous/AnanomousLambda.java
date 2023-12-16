a4f66beccb94a67f3a932476f53b4fc13c72619f
Amit Upadhyay <amitu2016@gmail.com>
2023-12-16 12:50:11 +0530

    commit check

--- Java8/Anonymous/AnanomousLambda.java
+++ Java8/Anonymous/AnanomousLambda.java
@@ -1,6 +1,7 @@
 package Java8.Anonymous;
 
 public class AnanomousLambda {
+    
     public static void main(String[] args) {
         Runnable r = () -> {
             for (int i = 0; i < 10; i++) {
