ee75129c8b09eb528e94b2a1310363681dc5cf71
Amit Upadhyay <amitu@cdac.in>
2023-12-25 12:41:09 +0530

    Function

--- Java8/Function/FunctionDemo1.java
+++ Java8/Function/FunctionDemo1.java
@@ -6,12 +6,20 @@ public class FunctionDemo1 {
     
     public static void main(String[] args) {
         //Java 8 Functional Interface (Function)
-        
+
         //Find length of given string
         Function<String, Integer> f = s -> s.length();
 
         System.out.println(f.apply("Amit"));
         System.out.println(f.apply("Upadhyay"));
+        System.out.println("**********************************************");
+
+        //Find square of given integer
+        Function<Integer, Integer> f1 = i -> i*i;
+        System.out.println(f1.apply(8));
+        System.out.println(f1.apply(9));
+        System.out.println("**********************************************");
+        
     }
 
 }
